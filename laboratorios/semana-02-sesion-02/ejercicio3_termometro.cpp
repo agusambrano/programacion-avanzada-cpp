fa3cef6d8c4857af721a8be35a0c3ef2049c7a8f
agusambrano <sambranoagustin5@gmail.com>
2026-08-26 13:08:05 +0000

    Semana-02-sesion-02 completada

--- laboratorios/semana-02-sesion-02/ejercicio3_termometro.cpp
+++ laboratorios/semana-02-sesion-02/ejercicio3_termometro.cpp
@@ -27,18 +27,17 @@ private:
 
 public:
     Termometro(double celsiusInicial) {
-        // TODO: si celsiusInicial es menor que -273.15, asigna
-        // gradosCelsius = 0.0 y avisa por consola:
-        // "Aviso: temperatura invalida, se uso 0 por defecto"
-        // Si no, asigna gradosCelsius = celsiusInicial.
-        //
-        // TODO: despues de decidir el valor, imprime
-        // "Termometro creado con " + gradosCelsius + " grados Celsius"
+        if (celsiusInicial < -273.15) {
+            gradosCelsius = 0.0;
+            std::cout << "Aviso: temperatura invalida, se uso 0 por defecto" << std::endl;
+        } else {
+            gradosCelsius = celsiusInicial;
+        }
+        std::cout << "Termometro creado con " << gradosCelsius << " grados Celsius" << std::endl;
     }
 
     ~Termometro() {
-        // TODO: imprime "Termometro destruido, ultima lectura: "
-        // seguido de gradosCelsius y " C".
+        std::cout << "Termometro destruido, ultima lectura: " << gradosCelsius << " C" << std::endl;
     }
 
     double getCelsius() {
@@ -46,6 +45,7 @@ public:
     }
 
     double convertirAFahrenheit() {
+        return gradosCelsius * 9.0 / 5.0 + 32.0;
         // TODO: retorna gradosCelsius convertido a Fahrenheit:
         // celsius * 9 / 5 + 32
         return 0.0;
